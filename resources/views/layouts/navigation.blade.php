06c6ccf81173c92ff1e6dde6c0f06f4e2ce08128
Bartomeu Mut Vidal <tomeu95@hotmail.com>
2023-02-01 18:50:19 +0100

    Create Views for Add Items

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,33 +20,33 @@
                             </x-nav-link>
                             <x-nav-link
                                 class="text-gray-300 hover:bg-[#3D8361] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                                :href="route('recipes')" :active="request()->routeIs('recipes')">
-                                {{ __('Recipes') }}
+                                :href="route('recipes')" :active="request()->routeIs('recipe')">
+                                {{ __('Recetas') }}
                             </x-nav-link>
                             <x-nav-link
                                 class="text-gray-300 hover:bg-[#3D8361] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                                :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                                {{ __('Grain') }}
+                                :href="route('grain')" :active="request()->routeIs('grain')">
+                                {{ __('Maltas') }}
                             </x-nav-link>
                             <x-nav-link
                                 class="text-gray-300 hover:bg-[#3D8361] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                                :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                                {{ __('Hops') }}
+                                :href="route('hop')" :active="request()->routeIs('hop')">
+                                {{ __('Lúpulos') }}
                             </x-nav-link>
                             <x-nav-link
                                 class="text-gray-300 hover:bg-[#3D8361] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                                :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                                {{ __('Yeast') }}
+                                :href="route('yeast')" :active="request()->routeIs('yeast')">
+                                {{ __('Levaduras') }}
                             </x-nav-link>
                             <x-nav-link
                                 class="text-gray-300 hover:bg-[#3D8361] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                                :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                                {{ __('Fermentables') }}
+                                :href="route('fermentable')" :active="request()->routeIs('fermentable')">
+                                {{ __('Fermentados') }}
                             </x-nav-link>
                             <x-nav-link
                                 class="text-gray-300 hover:bg-[#3D8361] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                                :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                                {{ __('Other Ingredients') }}
+                                :href="route('other_ingredients')" :active="request()->routeIs('other_ingredients')">
+                                {{ __('Otros Ingredientes') }}
                             </x-nav-link>
                         </div>
                     </div>
